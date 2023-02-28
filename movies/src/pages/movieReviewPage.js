e71e2ccf696f4f7ed8ba0197f53b98d1428fda18
unknown <chitasjessica@gmail.com>
2023-02-28 17:44:17 +0000

    Refactor - useMovie custom hook.

--- movies/src/pages/movieReviewPage.js
+++ movies/src/pages/movieReviewPage.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useParams } from 'react-router-dom';
+import MovieDetails from "../components/movieDetails/";
 import PageTemplate from "../components/templateMoviePage";
-import MovieReview from "../components/movieReview";
+import useMovie from "../hooks/useMovie";
 
-const MovieReviewPage = (props) => {
-  let location = useLocation();
-  const {movie, review} = location.state;
+const MoviePage = (props) => {
+  const { id } = useParams();
+  const [movie] = useMovie(id);
 
   return (
-    <PageTemplate movie={movie}>
-      <MovieReview review={review} />
-    </PageTemplate>
+    <>
+      {movie ? (
+        <>
+          <PageTemplate movie={movie}>
+            <MovieDetails movie={movie} />
+          </PageTemplate>
+        </>
+      ) : (
+        <p>Waiting for movie details</p>
+      )}
+    </>
   );
 };
 
-export default MovieReviewPage;
+export default MoviePage;
